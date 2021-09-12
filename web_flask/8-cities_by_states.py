08b71fae80f37541a150edb50a6a13a93434c2dd
jepez90 <ing.jersonperez@gmail.com>
2021-09-12 16:31:19 +0000

    [Add] files to show a list of cities by state

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,22 +1,25 @@
 #!/usr/bin/python3
 """ Script that starts a Flask web application  with a route:
-    /states_list:    render a template with all states in storage
+    /cities_by_states:    render a template with all states and its cities
 """
 
 from models import storage
 from models.state import State
+from models.city import City
 from flask import Flask
 from flask import render_template
 app = Flask(__name__)
 
 
-@app.route('/states_list', strict_slashes=False)
+@app.route('/cities_by_states', strict_slashes=False)
 def statesList():
-    """ handle the route /states_list and render a template """
+    """ handle the route /cities_by_states and render a template """
     all_states = storage.all(State).values()
     all_states = sorted(all_states, key=lambda state: state.name)
-
-    return render_template('7-states_list.html', states=all_states)
+    for state in all_states:
+        sorted_cities = sorted(state.cities, key=lambda city: city.name)
+        setattr(state, "sorted_cities", sorted_cities)
+    return render_template('8-cities_by_states.html', states=all_states)
 
 
 @app.teardown_appcontext
