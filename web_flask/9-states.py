f23c169bb0a60dc995f5df98576161c6d27404a0
jepez90 <ing.jersonperez@gmail.com>
2021-09-12 12:07:36 -0500

    [Add] file for task 10, show state by id

---
+++ web_flask/9-states.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+""" Script that starts a Flask web application  with a route:
+    /states:    render a template with all states in storage
+"""
+
+from models import storage
+from models.state import State
+from flask import Flask
+from flask import render_template
+app = Flask(__name__)
+
+
+@app.route('/states', strict_slashes=False)
+def statesList():
+    """ handle the route /states and render a template """
+    all_states = storage.all(State).values()
+    all_states = sorted(all_states, key=lambda state: state.name)
+
+    return render_template('7-states_list.html', states=all_states)
+
+
+@app.route('/states/<id>', strict_slashes=False)
+def statesCities(id):
+    """ handle the route /states/<id> and render a template """
+    all_states = storage.all(State)
+
+    state_id = "State." + id
+    if state_id in all_states:
+        state = all_states[state_id]
+        sorted_cities = sorted(state.cities, key=lambda city: city.name)
+        setattr(state, "sorted_cities", sorted_cities)
+    else:
+        state = None
+    print(state)
+
+    return render_template('9-states.html', state=state)
+
+
+@app.teardown_appcontext
+def teardown(self):
+    """ Removes the current SQLAlchemy Session """
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
